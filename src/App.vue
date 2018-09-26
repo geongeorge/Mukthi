f55c2a1121a0221e30768c9a20bfb045a49092bd
unknown <geongeorge98@gmail.com>
2018-09-26 16:20:37 +0530

    Added mapbox

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <mapbox
+ access-token="pk.eyJ1IjoiZ2Vvbmdlb3JnZWsiLCJhIjoiY2prZm5yaWZ5MDluODNrczdrMHRhZHB5eCJ9.xdRyfEKi2zGVLEUHIiVgFA"
+:map-options="{
+  style: 'mapbox://styles/mapbox/light-v9',
+  center: [-96, 37.8],
+  zoom: 3
+}"
+:geolocate-control="{
+  show: true,
+  position: 'top-left'
+}"
+:scale-control="{
+  show: true,
+  position: 'top-left'
+}"
+:fullscreen-control="{
+  show: true,
+  position: 'top-left'
+}">
+</mapbox>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Mapbox from 'mapbox-gl-vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Mapbox
   }
 }
 </script>
@@ -21,8 +41,12 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
+}
+#map {
+  width: 100%;
+  height: 500px;
 }
 </style>
